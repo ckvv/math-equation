8ff25d5ea1acb2d2772aec593277e9147c6d2688
chenkai0520 <chenk@geohey.com>
2019-07-22 21:10:33 +0800

    添加三元方程

--- src/components/MathEquation.vue
+++ src/components/MathEquation.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="math-equation-content">
         <div class="expression">
-            <el-input v-model="expression" clearable placeholder="sin(x); x*x; log(x); x+6">
+            <el-input v-model="expression" clearable placeholder="2x*sin(x); x*y;">
                 <template slot="prepend">方程</template>
             </el-input>
+            <el-slider v-model="intervalSlider" show-input></el-slider>
         </div>
         <div id="plot"></div>
     </div>
@@ -14,49 +15,124 @@
         compile,
         range
     } = require('mathjs');
-    let Plotly = require('plotly.js-dist');
+    let Plotly = require('plotly.js-dist/plotly');
+
     export default {
         name: 'mathEquation',
         data() {
             return {
-                expression: 'sin(x); x*cos(x); x+4',
+                expression: 'sin(0.5*x); min(x%2,0.5);',
+                intervalSlider: 12
             }
         },
         watch: {
-            expression(val){
-
-                this.drawExpression(val);
+            expression() {
+                this.drawExpression(this.expression);
+            },
+            intervalSlider() {
+                this.drawExpression(this.expression);
             }
         },
         mounted() {
             this.drawExpression(this.expression);
         },
         methods: {
-            drawExpression(val){
+            drawExpression(val) {
+                let tracesType = (val.indexOf('x') === -1 || val.indexOf('y') === -1) ? 'scatter' : 'scatter3d'
                 let expressions = val.split(';')
+
+                if (tracesType == 'scatter') {
+                    this.draw2DExpression(expressions, this.intervalSlider);
+                } else {
+                    this.draw3DExpression(expressions, this.intervalSlider);
+                }
+            },
+            draw2DExpression(expressions, intervalSlider) {
                 let traces = [];
-                
-                for(let expression of expressions){
-                    if(!expression) continue;
+                let intervalGap =  0.05;
+
+                for (let expression of expressions) {
+                    if (!expression) continue;
                     let expr;
                     try {
-                        expr = compile(expression)
+                        expr = compile(expression);
+                        const xValues = range(-intervalSlider, intervalSlider, intervalGap).toArray()
+                        const yValues = xValues.map(function (x) {
+                            return expr.evaluate({
+                                x: x
+                            })
+                        })
+
+                        traces.push({
+                            x: xValues,
+                            y: yValues,
+                            type: 'scatter',
+                            name: expression.trim()
+                        });
                     } catch (error) {
                         continue;
                     }
-                    const xValues = range(-12, 12, 0.01).toArray()
-                    const yValues = xValues.map(function (x) {
-                        return expr.evaluate({
-                            x: x
-                        })
-                    })
+                }
 
-                    traces.push({
-                        x: xValues,
-                        y: yValues,
-                        type: 'scatter',
-                        name: expression.trim()
-                    });
+                Plotly.newPlot('plot', traces);
+            },
+            draw3DExpression(expressions, intervalSlider) {
+                let traces = [];
+                let intervalGap = 0.05;
+                for (let expression of expressions) {
+                    if (!expression) continue;
+
+                    let haveX = expression.indexOf('x') !== -1;
+                    let haveY = expression.indexOf('y') !== -1;
+                    let expr;
+                    try {
+                        expr = compile(expression);
+                        const interval = range(-intervalSlider, intervalSlider, intervalGap).toArray();
+
+                        let xValues, yValues, zValues;
+                        if (haveX && haveY) {
+                            xValues = interval;
+                            yValues = interval;
+                            zValues = xValues.map(function (x) {
+                                return expr.evaluate({
+                                    x: x,
+                                    y: x
+                                })
+                            });
+                        }
+                        if (haveX && !haveY) {
+                            xValues = interval;
+                            yValues = interval.map(() => 0);
+                            zValues = xValues.map(function (x) {
+                                return expr.evaluate({
+                                    x: x,
+                                    y: 0,
+                                })
+                            });
+                        }
+                        if (haveY && !haveX) {
+                            xValues = interval.map(() => 0);
+                            yValues = interval;
+                            zValues = yValues.map(function (y) {
+                                return expr.evaluate({
+                                    x: 0,
+                                    y: y
+                                })
+                            });
+                        }
+
+                        traces.push({
+                            x: xValues,
+                            y: yValues,
+                            z: zValues,
+                            type: 'scatter3d',
+                            name: expression.trim()
+                        });
+
+                    } catch (error) {
+                        continue;
+                    }
+
                 }
 
                 Plotly.newPlot('plot', traces);
@@ -69,12 +145,14 @@
     .math-equation-content {
         width: 100%;
         height: 100%;
-        .expression{
+
+        .expression {
             position: fixed;
             top: 4px;
             left: 4px;
             z-index: 1000;
         }
+
         #plot {
             width: 100%;
             height: 100%;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
+import { Input, Slider } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
+Vue.component(Input.name, Input);
+Vue.component(Slider.name, Slider);
 
 Vue.config.productionTip = false
 
